0be33b8cf09210f75cd8d99198047cae2b85b669
Ryan Chhong <ryanchhong@gmail.com>
2020-09-06 14:37:47 -0500

    moved arrow keys around

--- keyboards/lily58/keymaps/InfernalHydra/keymap.c
+++ keyboards/lily58/keymaps/InfernalHydra/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT( \
-  KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQUAL, \
+  KC_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQUAL, \
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS, \
   KC_LCTRL, KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, \
@@ -58,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    |      | PgUp |      | PgDn |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |      |      |      |-------.    ,-------| Home | Left | Down | Up   | Right|  \   |
+ * |      | `    |      |      |      |      |-------.    ,-------| Home | Left | Down | Up   | Right|  \   |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------| End  |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -69,15 +69,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_LOWER] = LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
   _______, _______, _______, _______, _______, _______,                   _______, KC_PGUP, _______, KC_PGDN, _______, _______,\
-  _______, _______, _______, _______, _______, _______,                   KC_HOME, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_BSLASH,\
-  KC_LSFT, _______, _______, _______, _______, _______, _______, _______, KC_END, _______, _______, _______, _______, _______, \
+  _______, KC_GRAVE, _______, _______, _______, _______,                  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, KC_BSLASH,\
+  _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______, _______, \
                              _______, _______, _______, _______, _______,  _______, _______, _______\
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |                    |  F7  |  F8  |  F9  | F10  | F11  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | `    | Prev | Play | Next |      |      |                    |      |      |      |      |PrtSc | Pause|
+ * |      | Prev | Play | Next |      |      |                    |      |      |      |      |PrtSc | Pause|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      | VolD | VolU | Mute |      |-------.    ,-------| Home |      |      |      | Ins  | Del  |
  * |------+------+------+------+------+------|   [   |    |    ]  |------+------+------+------+------+------|
@@ -90,7 +90,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_RAISE] = LAYOUT( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, \
-  KC_GRAVE, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, _______, _______,                     _______, _______, _______, _______, KC_PSCREEN, KC_PAUSE, \
+  _______, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK, _______, _______,                     _______, _______, _______, _______, KC_PSCREEN, KC_PAUSE, \
   _______, _______, KC_AUDIO_VOL_DOWN, KC_AUDIO_VOL_UP, KC_AUDIO_MUTE, _______,                     KC_HOME, _______, _______, _______, KC_INSERT, KC_DELETE, \
   _______, _______, _______, _______, _______, _______,  _______, _______,  KC_END, _______, _______, _______, _______, _______, \
                              _______, _______, _______,  _______, _______,  _______, _______, _______ \
